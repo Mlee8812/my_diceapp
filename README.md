4f31c7480ceda774e3f98f24da7e6b9cca98c63d
Michael Lee <mlee@michaels-mbp-2.myfiosgateway.com>
2023-05-30 15:42:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# my_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application that rolls the dice.
+
+A few resources to get you started if this is your first Flutter project:
+
+
---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.cyan({super.key})
+      : color1 = Colors.white,
+        color2 = Colors.cyan;
+
+  final Color color1;
+  final Color color2;
+
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
